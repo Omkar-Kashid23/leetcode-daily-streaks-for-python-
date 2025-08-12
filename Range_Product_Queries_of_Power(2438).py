0e7d099e21bc06d8a3d480ee0ebffcc3ef13aaf7
Omkar Kashid <omkarkashid1168@gmail.com>
2025-08-12 21:08:45 +0530

    Update Range_Product_Queries_of_Power(2438).py

--- Range_Product_Queries_of_Power(2438).py
+++ Range_Product_Queries_of_Power(2438).py
@@ -1,40 +1,29 @@
-<<<<<<< HEAD
-MOD = 10**9 + 7
-
-=======
->>>>>>> 163ad7b648ffdac927109e2d44b7eb9272981358
 class Solution(object):
-    def productQueries(self, n, queries):
+    def numberOfWays(self, n, x):
         """
         :type n: int
-        :type queries: List[List[int]]
-        :rtype: List[int]
+        :type x: int
+        :rtype: int
         """
-        lst=[]
-        while n>0:
-            m=1
-            while n >= (m<<1):
-                m<<=1
-            lst.append(m)
-            n -= m
-        lst.sort()
-<<<<<<< HEAD
-        dp=[[0]*k for _ in range(k)]
-=======
-        k=len(lst)
-        MOD=10**9+7
->>>>>>> 163ad7b648ffdac927109e2d44b7eb9272981358
-        dp=[[0]*k for  _ in range(k)]
-        for i in range(k):
-            dp[i][i]=lst[i]
-        for i in range(k):
-            for j in range(i+1,k):
-                dp[i][j]=(dp[i][j-1]*lst[j])%MOD
-        ans=[]
-        for a,b in queries:
-            ans.append(dp[a][b])
-<<<<<<< HEAD
-        return ans
-=======
-        return ans
->>>>>>> 163ad7b648ffdac927109e2d44b7eb9272981358
+        MOD = 10**9+7  
+
+        # Precompute powers: 1^x, 2^x, ..., k^x <= n
+        powers = []
+        i = 1
+        while True:
+            p = i ** x
+            if p > n:
+                break
+            powers.append(p)
+            i += 1
+
+        # dp[s] = number of ways to make sum s using a subset of `powers`
+        dp = [0] * (n + 1)
+        dp[0] = 1  # one way to make 0: pick nothing
+
+        # 0/1 knapsack: iterate sums downward to avoid reusing the same power
+        for p in powers:
+            for s in range(n, p - 1, -1):
+                dp[s] = (dp[s] + dp[s - p]) % MOD
+
+        return dp[n]
