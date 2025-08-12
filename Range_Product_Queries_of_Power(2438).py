742c571175e8521984f1933dd5e2b24186fb58e2
Omkar-Kashid23 <omkarkashid1168@gmail.com>
2025-08-12 08:56:27 +0530

    Implement productQueries function for range product queries of powers

---
+++ Range_Product_Queries_of_Power(2438).py
@@ -0,0 +1,28 @@
+MOD = 10**9 + 7
+
+class Solution(object):
+    def productQueries(self, n, queries):
+        """
+        :type n: int
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        lst=[]
+        while n>0:
+            m=1
+            while n >= (m<<1):
+                m<<=1
+            lst.append(m)
+            n -= m
+        lst.sort()
+        dp=[[0]*k for _ in range(k)]
+        dp=[[0]*k for  _ in range(k)]
+        for i in range(k):
+            dp[i][i]=lst[i]
+        for i in range(k):
+            for j in range(i+1,k):
+                dp[i][j]=(dp[i][j-1]*lst[j])%MOD
+        ans=[]
+        for a,b in queries:
+            ans.append(dp[a][b])
+        return ans
