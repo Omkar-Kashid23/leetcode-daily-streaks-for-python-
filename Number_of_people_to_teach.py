63b8218d5f1efd8530591c3cb083ea37aa0a766a
Omkar Kashid <omkarkashid1168@gmail.com>
2025-09-10 21:53:10 +0530

    Create Number_of_people_to_teach.py

---
+++ Number_of_people_to_teach.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        learned = list(map(set, languages))
+
+        total = 0
+        vis = [False] * len(languages)
+        cnt = [0] * (n + 1)
+
+        def add(u: int) -> None:
+            if vis[u]:
+                return
+            vis[u] = True  
+            nonlocal total
+            total += 1
+            for x in languages[u]:
+                cnt[x] += 1
+
+        for u, v in friendships:
+            if learned[u - 1].isdisjoint(learned[v - 1]):
+                add(u - 1)
+                add(v - 1)
+
+        return total - max(cnt)
