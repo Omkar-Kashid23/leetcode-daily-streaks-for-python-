78f0f6d79975abc27e24adbec642ac3e69540fc5
Omkar Kashid <omkarkashid1168@gmail.com>
2025-09-15 20:18:28 +0530

    Create Maximum_number_of_words_you_can_type.py

---
+++ Maximum_number_of_words_you_can_type.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        counter = 0
+        for word in text.split():
+            for letter in word:
+                if letter in brokenLetters:
+                    counter+=1
+                    break
+        return len(text.split())-counter
+        
