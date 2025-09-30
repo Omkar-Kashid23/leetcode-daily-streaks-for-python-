a1a3f16c842945a468939442114d608817c69a3a
Omkar Kashid <omkarkashid1168@gmail.com>
2025-09-30 20:06:18 +0530

    Update Trinagular_sum_arr.py

--- Trinagular_sum_arr.py
+++ Trinagular_sum_arr.py
@@ -5,4 +5,16 @@ class Solution:
             for i in range(len(nums)-1):n_arr.append((nums[i]+nums[i+1])%10)
             nums = n_arr
         return nums[0]
-            
+
+
+#                OR
+
+
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        res = 0
+        n = len(nums)
+        for i, num in enumerate(nums):
+            res += num * comb(n-1, i) 
+
+        return res % 10
