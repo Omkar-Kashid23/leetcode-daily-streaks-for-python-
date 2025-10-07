5f45d3e5c3de86f625b52134ff812e5c57358a6b
Omkar Kashid <omkarkashid1168@gmail.com>
2025-10-07 18:49:00 +0530

    Create Avoid_flood_in_the_city.py

---
+++ Avoid_flood_in_the_city.py
@@ -0,0 +1,14 @@
+class Solution:
+    def avoidFlood(self, rains: List[int]) -> List[int]:
+        f, stk, res = {}, [], [1] * len(rains)
+        for i, v in enumerate(rains):
+            if v == 0:
+                stk.append(i)
+                continue
+            res[i] = -1
+            if v in f: 
+                if (p := bisect.bisect(stk, f[v])) == len(stk):
+                    return []
+                res[stk.pop(p)] = v
+            f[v] = i
+        return res 
