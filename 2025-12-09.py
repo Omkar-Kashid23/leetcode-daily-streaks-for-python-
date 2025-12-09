358c5833abc15f64f7a3bbbbfb1365850ad0c642
Omkar-Kashid23 <omkarkashid1168@gmail.com>
2025-12-09 21:41:57 +0530

    Daily update

---
+++ 2025-12-09.py
@@ -0,0 +1,19 @@
+class Solution:
+    def specialTriplets(self, nums: List[int]) -> int:
+        dic={}
+        dic2={}
+        res=0
+        for num in nums:
+            if num%2==0 and num//2 in dic2:
+                res+=dic2[num//2]
+                res%=10**9+7
+            if num*2 in dic:
+                if num in dic2:
+                    dic2[num]+=dic[num*2]
+                else:
+                    dic2[num]=dic[num*2]
+            if num in dic:
+                dic[num]+=1
+            else:
+                dic[num]=1
+        return res
