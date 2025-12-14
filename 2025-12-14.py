c4a5f0ca7b4be288ae0a9525ead599bd75d70a8b
Omkar-Kashid23 <omkarkashid1168@gmail.com>
2025-12-14 11:14:53 +0530

    Daily update

---
+++ 2025-12-14.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numberOfWays(self, corridor: str) -> int:
+        mod = 10**9 + 7
+        total_seats = corridor.count("S")
+        if total_seats == 0 or total_seats %2 == 1:
+            return 0
+        ans,seat,plant = 1,0,0
+        for c in corridor:
+            if c == "S":
+                if seat == 2:
+                    ans = ans * (plant + 1)%mod
+                    plant = 0
+                    seat = 0
+                seat += 1
+            else:
+                if seat == 2:
+                    plant += 1
+        return ans
