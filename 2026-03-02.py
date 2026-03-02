32b293c28f306ea73a6412089a17fe41f8af15d2
Omkar Kashid <omkarkashid1168@gmail.com>
2026-03-02 19:10:41 +0530

    Create 2026-03-02.py

---
+++ 2026-03-02.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minSwaps(self, grid: List[List[int]]) -> int:
+        m,n  = len(grid),len(grid[0])
+        row = [0]*m
+        for i in range(m):row[i] = next((j for j in reversed(range(n))if grid[i][j]),0) 
+        ans = 0
+        for k in range(m):
+            for i,v in enumerate(row):
+                if v<= k:
+                    ans += i
+                    row.pop(i)
+                    break
+            else:return -1
+        return ans
