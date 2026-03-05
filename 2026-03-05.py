3b3890f6e5d712767c542951993025f02a3b4ceb
Omkar Kashid <omkarkashid1168@gmail.com>
2026-03-05 13:11:49 +0530

    Implement minOperations method in Solution class

---
+++ 2026-03-05.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minOperations(self, s: str) -> int:
+        cnt = 0
+        for i in range(len(s)):
+            if i % 2 == 0:
+                if s[i] != '0':cnt += 1
+            else:
+                if s[i] != '1':cnt += 1
+        return min(cnt,len(s)-cnt)
