29ffbcd396790b620d8e7bfad27f2a1a7d760d86
Omkar Kashid <omkarkashid1168@gmail.com>
2026-06-08 13:14:57 +0530

    Implement pivotArray function to rearrange list

---
+++ 2026-06-08.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        lst1 = []
+        lst2 = []
+        lst3 = []
+        for i in nums:
+            if i < pivot:lst1.append(i)
+            elif i == pivot:lst3.append(i)
+            else:lst2.append(i)
+        return lst1 + lst3 + lst2
