eca0ec07727a02be7688502fbb715bf81ec58f20
Omkar Kashid <omkarkashid1168@gmail.com>
2026-09-07 17:46:27 +0530

    Implement distinct subsequences calculation

---
+++ 07-09-2026.py
@@ -0,0 +1,11 @@
+class Solution:
+    def distinctSubseqII(self, s: str) -> int:
+        end = [0] * 26
+        total = 0
+        MOD = 10 ** 9 + 7
+        for ch in s:
+            idx = ord(ch) - 97
+            newEnd = (total + 1) % MOD
+            total = (total + newEnd - end[idx]) % MOD
+            end[idx] = newEnd
+        return total
